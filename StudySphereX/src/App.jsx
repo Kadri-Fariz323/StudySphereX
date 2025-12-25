0feeaa37f0053c7517d70c11be8149b2f1ee84c3
Fariz <farizkadri3@gmail.com>
2025-12-25 02:08:15 -1200

    Created admin routes and a dashboard page

--- StudySphereX/src/App.jsx
+++ StudySphereX/src/App.jsx
@@ -1,25 +1,31 @@
 import { Route, Routes } from "react-router-dom";
 import { PublicLayout } from "./layouts/PublicLayout";
+import { AdminPanelLayout } from "./layouts/AdminPanelLayout";
 
 import { Login } from "./pages/Login";
-import { Home } from './pages/Home'
-import { Contact } from './pages/Contact'
+import { Home } from "./pages/Home";
+import { Contact } from "./pages/Contact";
 import { Courses } from "./pages/Courses";
+
+import { AdminDashboard } from "./pages/admin/adminDashboard";
 
 export const App = () => {
   return (
-   
-        <Routes>
-        {/* Public Routes */}
-          <Route element={<PublicLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/courses" element={<Courses />} />
-            <Route path="/contact" element={<Contact />} />
+    <>
+      <Routes>
+        {/* Public Home page Routes */}
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
 
-          </Route>
-        
-        </Routes>
-    
+        {/* Admin */}
+        <Route path="/admin" element={<AdminPanelLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
