1e19a801cac764727250f17d86f24933be67af03
Fariz <farizkadri3@gmail.com>
2025-12-25 02:11:44 -1200

    Created a callToAction UI component

--- StudySphereX/src/pages/Home.jsx
+++ StudySphereX/src/pages/Home.jsx
@@ -1,20 +1,19 @@
-import { BenefitsSection } from '../components/HomePage/BenefitsSection'
-import { CallToAction } from '../components/HomePage/CallToAction'
-import { FeaturedCourses } from '../components/HomePage/FeaturedCourses'
-import { Hero } from '../components/HomePage/Hero'
-import { PopularCourses } from '../components/HomePage/PopularCourses'
-import { Working } from '../components/HomePage/Working'
+import { BenefitsSection } from "../components/HomePage/BenefitsSection";
+import { CallToAction } from "../components/HomePage/CallToAction";
+import { FeaturedCourses } from "../components/HomePage/FeaturedCourses";
+import { Hero } from "../components/HomePage/Hero";
+import { PopularCourses } from "../components/HomePage/PopularCourses";
+import { Working } from "../components/HomePage/Working";
 
 export const Home = () => {
   return (
     <>
-     <Hero />
-    <Working />
-    <PopularCourses />
-    <FeaturedCourses />
-    <BenefitsSection />
-    <CallToAction />
+      <Hero />
+      <Working />
+      <PopularCourses />
+      <FeaturedCourses />
+      <BenefitsSection />
+      <CallToAction />
     </>
-   
-  )
-}
+  );
+};
