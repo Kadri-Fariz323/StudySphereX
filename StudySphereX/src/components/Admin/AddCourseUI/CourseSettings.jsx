806f178064efb60e97b9e642e803c6e2decaa0c9
Fariz <farizkadri3@gmail.com>
2025-12-29 03:06:25 -1200

    Created Basic structure for CourseSettings page

--- StudySphereX/src/components/Admin/AddCourseUI/CourseSettings.jsx
+++ StudySphereX/src/components/Admin/AddCourseUI/CourseSettings.jsx
@@ -2,8 +2,16 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export const CourseSettings = () => {
   return (
-    <div>
-        
-    </div>
+    <Card>
+         <CardHeader>
+          <CardTitle>Course Settings</CardTitle>
+         </CardHeader>
+         <CardContent>
+          <div className="flex flex-col gap-3">
+            <label htmlFor="Thumbnail">Upload Course Thumbnail</label>
+            <input type="file" name="Thumbnail" accept="image/*" className="mb-4" />
+          </div>
+         </CardContent>
+    </Card>
   )
 }
