7af49fda90e3bf377a3b695de1e75ce212bb8fbe
Fariz <farizkadri3@gmail.com>
2026-01-07 05:49:33 -1200

    Created Course controllers Route file

---
+++ server/model/Course.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+// 1. Define QuizSchema  
+const QuizSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  questions: [
+    {
+      question: { type: String, required: true },
+      options: [String],
+      correctAnswerIndex: { type: Number, required: true },
+    },
+  ],
+  passingMarks: { type: Number, default: 70 }, 
+});
+
+// 2.  LectureSchema 
+const LectureSchema = new mongoose.Schema({
+  title: String,
+  videoUrl: String,
+  public_id: String,
+  freePreview: Boolean,
+  pdfUrl: String,
+  pdfPublicId: String,
+  quiz: QuizSchema, 
+});
+
+// 3. Define CourseSchema 
+const CourseSchema = new mongoose.Schema({
+  instructorId: String,
+  instructorName: String,
+  date: Date,
+  title: String,
+  category: String,
+  level: String,
+  primaryLanguage: String,
+  subtitle: String,
+  description: String,
+  image: String,
+  welcomeMessage: String,
+  pricing: Number,
+  objectives: String,
+  students: [
+    {
+      studentId: String,
+      studentName: String,
+      studentEmail: String,
+      paidAmount: String,
+    },
+  ],
+  curriculum: [LectureSchema],
+  
+  isPublished: Boolean, 
+
+  finalQuiz: {
+    type: QuizSchema,
+    default: null
+  },
+});
+
+module.exports = mongoose.model("Course", CourseSchema);
