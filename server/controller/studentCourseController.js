4db38fe33ed1b944c8835984d7a4942188d84719
Fariz <farizkadri3@gmail.com>
2026-01-19 04:48:46 -1200

    Created API to fetch purchased courses by students

---
+++ server/controller/studentCourseController.js
@@ -0,0 +1,32 @@
+const StudentCourses = require("../model/StudentCourses");
+
+exports.getCoursesByStudentId = async (req, res) => {
+  try {
+    const { studentId } = req.params;
+
+    const studentBoughtCourses = await StudentCourses.findOne({
+      userId: studentId,
+    });
+
+    
+    
+    if (!studentBoughtCourses) {
+      return res.status(200).json({
+        success: true,
+        data: [], 
+      });
+    }
+    
+
+    res.status(200).json({
+      success: true,
+      data: studentBoughtCourses.courses,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured!",
+    });
+  }
+};
